ae3b1e2a53b2a50f00f71d479b0ef75e2198a4dc
Nishitha <nishithasurapudi@gmail.com>
2020-07-03 15:18:20 +0530

    added tap-place to pinch-scale

--- js/pinch-scale.js
+++ js/pinch-scale.js
@@ -21,3 +21,25 @@ AFRAME.registerComponent('pinch-scale', {
     this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z
   }
 })
+
+// Component that places model where the ground is clicked
+AFRAME.registerComponent('tap-place', {
+  init: function () {
+      const ground = document.getElementById('ground')
+      ground.addEventListener('click', event => {
+              // Create new entity for the new object
+              const newElement = document.createElement('a-entity')
+              // The raycaster gives a location of the touch in the scene
+              const touchPoint = event.detail.intersection.point
+              newElement.setAttribute('position', touchPoint)
+              newElement.setAttribute('visible', 'false')
+              newElement.setAttribute('scale', '0.3 0.3 0.3')
+              newElement.setAttribute('gltf-model', '#model')
+              this.el.sceneEl.appendChild(newElement)
+              newElement.addEventListener('model-loaded', () => {
+                  newElement.setAttribute('visible', 'true')
+              })
+      })
+  }
+})
+
