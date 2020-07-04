8a6a4949a92ad10e22eb77dfd77c3eab739b3a0d
Nishitha <nishithasurapudi@gmail.com>
2020-07-04 14:35:57 +0530

    ww

--- js/camera.js
+++ js/camera.js
@@ -34,13 +34,17 @@ AFRAME.registerComponent('two-finger-spin', {
     init: function() {
       this.handleEvent = this.handleEvent.bind(this)
       this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
-      this.el.sceneEl.addEventListener('rightswipe',this.handleevent)
-      this.el.sceneEl.addEventListener('leftswipe',this.handleEvent)
+      this.el.sceneEl.addEventListener('onefingerswipe', this.handleEvent)
+
+      //this.el.sceneEl.addEventListener('rightswipe',this.handleevent)
+      //this.el.sceneEl.addEventListener('leftswipe',this.handleEvent)
     },
     remove: function() {
       this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
-      this.el.sceneEl.addEventListener('rightswipe',this.handleevent)
-      this.el.sceneEl.addEventListener('leftswipe',this.handleEvent)
+      this.el.sceneEl.removeEventListener('twofingerswipe', this.handleEvent)
+
+      //this.el.sceneEl.addEventListener('rightswipe',this.handleevent)
+      //this.el.sceneEl.addEventListener('leftswipe',this.handleEvent)
 
     },
     handleEvent: function(event) {
