be9428d5df01d4f1398bc96f988bcf27ae3a5a69
Nishitha <nishithasurapudi@gmail.com>
2020-07-04 13:52:42 +0530

    added isplaced boolean

--- js/camera.js
+++ js/camera.js
@@ -28,3 +28,34 @@ AFRAME.registerComponent('tap-place', {
     }
 
 })
+AFRAME.registerComponent('pinch-scale', {
+    schema: {
+        min: { default: 5 },
+        max: { default: 100 }
+    },
+
+    init: function () {
+        this.initialScale = this.el.object3D.scale.clone()
+        this.scaleFactor = 1
+        this.handleEvent = this.handleEvent.bind(this)
+        this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+    },
+
+    remove: function () {
+        this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+    },
+
+    handleEvent: function (event) {
+        this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread;
+        this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.min), this.data.max);
+        var gltfModel;
+        if (isplaced) {
+            gltfModel = document.getElementById('model');
+        }
+
+        
+        gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
+        gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
+        gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
+    }
+});
