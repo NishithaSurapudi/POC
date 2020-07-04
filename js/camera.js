8ec3d250822720427bf11f473e3e2adf3f330250
Nishitha <nishithasurapudi@gmail.com>
2020-07-04 14:26:12 +0530

    ee

--- js/camera.js
+++ js/camera.js
@@ -22,11 +22,31 @@ AFRAME.registerComponent('tap-place', {
                         clip: 'model',
                         loop: 'once'
                     })
-                })
+                });
             }
         })
     }
-})
+});
+AFRAME.registerComponent('two-finger-spin', {
+    schema: {
+      factor: {default: 5}
+    },
+    init: function() {
+      this.handleEvent = this.handleEvent.bind(this)
+      this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+      this.el.sceneEl.addEventListener('rightswipe',this.handleevent)
+      this.el.sceneEl.addEventListener('leftswipe',this.handleEvent)
+    },
+    remove: function() {
+      this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+      this.el.sceneEl.addEventListener('rightswipe',this.handleevent)
+      this.el.sceneEl.addEventListener('leftswipe',this.handleEvent)
+
+    },
+    handleEvent: function(event) {
+      this.el.object3D.rotation.y += event.detail.positionChange.x * this.data.factor
+    }
+  });
 AFRAME.registerComponent('pinch-scale', {
     schema: {
         min: { default: 5 },
