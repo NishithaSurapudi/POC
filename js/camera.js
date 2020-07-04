7943409cab593959b960ea6908de8f7b94863ca7
Nishitha <nishithasurapudi@gmail.com>
2020-07-04 13:39:07 +0530

    rr

--- js/camera.js
+++ js/camera.js
@@ -6,8 +6,11 @@
                 const ground = document.getElementById('ground')
                 ground.addEventListener('click', event => {
                     // Create new entity for the new object
-                    const newElement = document.createElement('a-entity')
-                    // The raycaster gives a location of the touch in the scene
+                    if(!isplaced){
+                        const newElement = document.createElement('a-entity')
+                            newElement.setAttribute('isplaced','true')
+                        
+                    }                    // The raycaster gives a location of the touch in the scene
                     const touchPoint = event.detail.intersection.point
                     newElement.setAttribute('position', touchPoint)
                     newElement.setAttribute('visible', 'false')
@@ -15,11 +18,7 @@
                     newElement.setAttribute('gltf-model', '#model')
                     this.el.sceneEl.appendChild(newElement)
                     //newElement.addEventListener('model-loaded', () => {
-                        if(!isplaced){
-                            const newElement = document.createElement('a-entity')
-                                newElement.setAttribute('isplaced','true')
-                            
-                        }
+                        
                         newElement.setAttribute('visible', 'true')
                         newElement.setAttribute('Animation-mixer',{
                             clip:'model',
