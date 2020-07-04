0b769dfb0d4a5bd4d2256e305f08be6cfc9acd28
Nishitha <nishithasurapudi@gmail.com>
2020-07-04 13:46:01 +0530

    ww

--- js/camera.js
+++ js/camera.js
@@ -1,32 +1,30 @@
-        // Component that places model where the ground is clicked
-        var isplaced= false;
+// Component that places model where the ground is clicked
+var isplaced = false;
 
-        AFRAME.registerComponent('tap-place', {
-            init: function() {
-                const ground = document.getElementById('ground')
-                ground.addEventListener('click', event => {
-                    // Create new entity for the new object
-                    if(!isplaced){
-                        const newElement = document.createElement('a-entity')
-                            newElement.setAttribute('isplaced','true')
-                        
-                                     
-                     // The raycaster gives a location of the touch in the scene
-                    const touchPoint = event.detail.intersection.point
-                    newElement.setAttribute('position', touchPoint)
-                    newElement.setAttribute('visible', 'false')
-                    newElement.setAttribute('scale', '0.3 0.3 0.3')
-                    newElement.setAttribute('gltf-model', '#model')
-                    this.el.sceneEl.appendChild(newElement)
-                    //newElement.addEventListener('model-loaded', () => {
-                    } 
-                        newElement.setAttribute('visible', 'true')
-                        newElement.setAttribute('Animation-mixer',{
-                            clip:'model',
-                            loop:'once'
-                        })
-                        })
-                //})
+AFRAME.registerComponent('tap-place', {
+    init: function () {
+        const ground = document.getElementById('ground')
+        ground.addEventListener('click', event => {
+            // Create new entity for the new object
+            if (!isplaced) {
+                isplaced = true;
+                const newElement = document.createElement('a-entity')
+                // The raycaster gives a location of the touch in the scene
+                const touchPoint = event.detail.intersection.point
+                newElement.setAttribute('position', touchPoint)
+                newElement.setAttribute('visible', 'false')
+                newElement.setAttribute('scale', '0.3 0.3 0.3')
+                newElement.setAttribute('gltf-model', '#model')
+                this.el.sceneEl.appendChild(newElement)
+                //newElement.addEventListener('model-loaded', () => {
+                newElement.setAttribute('visible', 'true')
+                newElement.setAttribute('Animation-mixer', {
+                    clip: 'model',
+                    loop: 'once'
+                })
             }
-            
+
         })
+    }
+
+})
