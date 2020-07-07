a2a1831f84d054c02201ce2df0ae748451c49306
Nishitha <nishithasurapudi@gmail.com>
2020-07-07 20:26:55 +0530

    66

--- js/camera.js
+++ js/camera.js
@@ -13,6 +13,7 @@ AFRAME.registerComponent('tap-place', {
                 isplaced = true;
                 const newElement = document.createElement('a-entity')
                 const xrweb = document.getElementsByTagName('a-scene')
+                console.log(xrweb)
                 // The raycaster gives a location of the touch in the scene
                 const touchPoint = event.detail.intersection.point
                 newElement.setAttribute('position', touchPoint)
