a40a07af6d34c764181a797abf48969dc6b4aefb
Nishitha <nishithasurapudi@gmail.com>
2020-07-09 06:12:54 +0530

    ww

--- js/camera.js
+++ js/camera.js
@@ -26,7 +26,7 @@ AFRAME.registerComponent('tap-place', {
                 this.el.sceneEl.appendChild(newElement)
                 newElement.addEventListener('model-loaded', () => {
                     newElement.setAttribute('visible', 'true')
-                    newElement.setTimeout(() =>{
+                    this.el.setTimeout(() =>{
                         newElement.setAttribute('Animation-mixer', {
                             clip: 'model'
                         },5000)
